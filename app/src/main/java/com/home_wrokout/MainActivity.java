9600aec5d2287227e73f2323f28ce45715f3cc6d
Jakyion <patrykr5@onet.pl>
2021-05-07 16:42:53 +0200

    dodano plan cwiczen

--- app/src/main/java/com/home_wrokout/MainActivity.java
+++ app/src/main/java/com/home_wrokout/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.home_wrokout.Alarm_RecyclerView.Alarm_activity;
+
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button plan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button=findViewById(R.id.button12);
+        plan=findViewById(R.id.button10);
+        plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlarm_activity();
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openTreningiActivity(){
         Intent intent = new Intent(this, TreningiActivity.class);
+        startActivity(intent);
+    }
+    public void openAlarm_activity(){
+        Intent intent = new Intent(this, Alarm_activity.class);
         startActivity(intent);
     }
 }
