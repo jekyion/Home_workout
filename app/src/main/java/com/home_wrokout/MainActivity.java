78278d65bffe8c0e888b83f5e69e8822120c2e95
Jakyion <patrykr5@onet.pl>
2021-05-07 15:49:17 +0200

    yep

--- app/src/main/java/com/home_wrokout/MainActivity.java
+++ app/src/main/java/com/home_wrokout/MainActivity.java
@@ -2,13 +2,29 @@ package com.home_wrokout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button12);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openTreningiActivity();
+            }
+        });
+    }
+    public void openTreningiActivity(){
+        Intent intent = new Intent(this, TreningiActivity.class);
+        startActivity(intent);
     }
 }
