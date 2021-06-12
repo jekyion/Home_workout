4c940c7376cc6e04dd637f34fca458a9b80ca505
Jakyion <patrykr5@onet.pl>
2021-06-12 15:07:42 +0200

    wszystkie cwiczenia dzialaja

---
+++ app/src/main/java/com/home_wrokout/KlataPozActivity.java
@@ -0,0 +1,112 @@
+package com.home_wrokout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class KlataPozActivity extends AppCompatActivity {
+
+    private Button pocz;
+    private Button sred;
+    private Button zaw;
+    // String[] cwp={"1","2","3","4","5"};
+    private ArrayList<String> cwp = new ArrayList<String>();
+    private ArrayList<Integer> icp = new ArrayList<>();
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_klata_poz);
+
+
+
+
+        pocz=findViewById(R.id.klatapocz);
+        pocz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listAddPocz();
+                openExTimerActivity();
+            }
+        });
+        sred=findViewById(R.id.klatasred);
+        sred.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listAddSred();
+                openExTimerActivity();
+            }
+        });
+        zaw=findViewById(R.id.klatazaaw);
+        zaw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listAddZaw();
+                openExTimerActivity();
+            }
+        });
+    }
+    public void openExTimerActivity(){
+        Bundle b=new Bundle();
+        Bundle c=new Bundle();
+        b.putStringArrayList("cwp",cwp);
+        c.putIntegerArrayList("icp",icp);
+        Intent i = new Intent(this, ExTimerActivity.class);
+        i.putExtras(b);
+        i.putExtras(c);
+        startActivity(i);
+    }
+    public void listAddPocz(){
+        cwp.clear();
+        cwp.add("JUMPING JACKS");
+        cwp.add("ABDOMINAL CRUNCHES");
+        cwp.add("MOUNTAIN CLIMBER");
+        cwp.add("HEEL TOUCH");
+        cwp.add("PLANK");
+
+        icp.clear();
+        icp.add(R.drawable.jumpingjacks);
+        icp.add(R.drawable.abdominalcrunches);
+        icp.add(R.drawable.mountainclimber);
+        icp.add(R.drawable.heeltouch);
+        icp.add(R.drawable.plank);
+    }
+    public void listAddSred(){
+        cwp.clear();
+        cwp.add("HEEL TOUCH");
+        cwp.add("MOUNTAIN CLIMBER");
+        cwp.add("LEG RAISES");
+        cwp.add("BUTT BRIDGE");
+        cwp.add("PLANK");
+
+        icp.clear();
+        icp.add(R.drawable.heeltouch);
+        icp.add(R.drawable.mountainclimber);
+        icp.add(R.drawable.legraises);
+        icp.add(R.drawable.buttbridge);
+        icp.add(R.drawable.plank);
+    }
+    public void listAddZaw(){
+        cwp.clear();
+        cwp.add("SIT-UPS");
+        cwp.add("COBRA STRETCH");
+        cwp.add("BICYCLE CRUNCHES");
+        cwp.add("ABDOMINAL CRUNCHES");
+        cwp.add("PLANK");
+
+        icp.clear();
+        icp.add(R.drawable.situp);
+        icp.add(R.drawable.cobrastretch);
+        icp.add(R.drawable.bicyclecrunches);
+        icp.add(R.drawable.abdominalcrunches);
+        icp.add(R.drawable.plank);
+    }
+}
--- app/src/main/java/com/home_wrokout/TreningiActivity.java
+++ app/src/main/java/com/home_wrokout/TreningiActivity.java
@@ -8,22 +8,68 @@ import android.view.View;
 import android.widget.Button;
 
 public class TreningiActivity extends AppCompatActivity {
-    private Button button;
+    private Button brzuch;
+    private Button klata;
+    private Button ramiona;
+    private Button nogi;
+    private Button plecy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treningi);
 
-        button=findViewById(R.id.button5);
-        button.setOnClickListener(new View.OnClickListener() {
+        brzuch=findViewById(R.id.button5);
+        klata=findViewById(R.id.button4);
+        ramiona=findViewById(R.id.button);
+        nogi=findViewById(R.id.button2);
+        plecy=findViewById(R.id.button3);
+        brzuch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {openBrzuchPozActivity();}
+        });
+
+        klata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openKlataPozActivity();}
+        });
+
+        ramiona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openRamionaPozActivity();}
+        });
+
+        nogi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openNogiPozActivity();}
+        });
+
+        plecy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openPlecyPozActivity();}
         });
 
     }
     public void openBrzuchPozActivity(){
         Intent intent = new Intent(this, BrzuchPozActivity.class);
+        startActivity(intent);
+    }
+    public void openKlataPozActivity(){
+        Intent intent = new Intent(this,KlataPozActivity.class);
+        startActivity(intent);
+    }
+    public void openRamionaPozActivity(){
+        Intent intent = new Intent(this,RamionaPozActivity.class);
+        startActivity(intent);
+    }
+
+    public void openNogiPozActivity(){
+        Intent intent = new Intent(this,NogiPozAzctivity.class);
+        startActivity(intent);
+    }
+
+    public void openPlecyPozActivity(){
+        Intent intent = new Intent(this,PlecyPozActivity.class);
         startActivity(intent);
     }
 }
